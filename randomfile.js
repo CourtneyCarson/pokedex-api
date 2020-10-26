2b9c0833268c2c8510c2b69d7775d3385c606598
Courtney Carson <cmacarson@gmail.com>
2020-10-25 18:18:40 -0700

    prep for heroku

--- randomfile.js
+++
@@ -1,5 +0,0 @@
-app.get('hello', (req, res) => {
-  res 
-    .status(500)
-  .send('Oops, I did it again.')
-})
--- server.js
+++ server.js
@@ -1,48 +1,66 @@
-const express = require('express')
-const morgan = require('morgan')
-console.log(process.env.API_TOKEN)
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
+const app = express();
 
-require('dotenv').config()
-const app = express()
-
-app.use(morgan('dev'))
-
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`,
-  `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`,
-  `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-  console.log('validate bearer token middlewear')
-  debugger
-  // move to next middleware
-  next()
-})
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
+const validTypes = [
+  'Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire',
+  'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison',
+  'Psychic', 'Rock', 'Steel', 'Water',
+];
 
 function handleGetTypes(req, res) {
-res.send(validTypes)
+  res.json(validTypes);
 }
-
-///                 middleware, middleware2, middleware3 etc.
-// app.get(/types, validateBearerToken, handleGetTypes);
-// app.get('/pokemon', validateBearerToken, handleGetPokemon);
-
-// This approach is better:
-// app.use(validateBearerToken);
 app.get('/types', handleGetTypes);
-// app.get('/pokemon', handleGetPokemon);
 
-function handleGetPokemon(res, req) {
-  res.send('Hello, Pokemon!')
+function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      pokemon.name
+        .toLocaleLowerCase()
+        .includes(req.query.name.toLocaleLowerCase())
+    );
+  }
+
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+  res.json(response);
 }
 
 app.get('/pokemon', handleGetPokemon);
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
-const PORT = 8000
-
-app.listen(PORT, cb)
-
-// app.listen(PORT, cb () => {
-//   console.log(`Server listening at http://localhost:${PORT}`)
-// })
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => { });
